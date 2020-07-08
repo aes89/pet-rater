b79b484fe7ed1dba2b20e5898104f82661f9d258
aes89 <adrienne.e.smith@outlook.com>
2020-07-08 18:54:52 +1000

    Add Pet Form Fields: Name, Species

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,4 +1,37 @@
 class PetsController < ApplicationController
     def index
+        @pets = Pet.all
     end
+
+    def new
+        @pets = Pet.new
+    end
+
+    def create
+        @pets = Pet.create(pets_params)
+        if @pets.errors.any?
+            set_sexes
+            render "new"
+        # need to say "render "new"", using just "new" is calling the whole method.
+        else 
+            redirect_to pets_path
+        end 
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+    def set_sexes
+        @sexes = Pet.sexes.keys
+     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
 root "pages#index"
 resources :pets
 
+# get "/pets", to: "pets#index", as: "pets"
+# post "/pets", to: "pets#create"
+# get "/pets/new", to: "pets#new", as: "new_listing"
+# get "/pets/:id", to: "pets#show", as: "listing"
+# put "/pets/:id", to: "pets#update"
+# patch "/pets/:id", to: "pets#update"
+# delete "/pets/:id", to: "pets#destroy"
+# get "/pets/:id/edit", to: "pets#edit", as: "edit_listing"
 
 end
