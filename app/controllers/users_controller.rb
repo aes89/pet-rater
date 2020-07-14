aa699bc0d183ac7f74e41f4c187bed1e60bce7f8
aes89 <adrienne.e.smith@outlook.com>
2020-07-14 13:59:36 +1000

    Nav Style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -68,6 +68,7 @@ class UsersController < ApplicationController
     end
 
     # Only allow a list of trusted parameters through.
+        # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.fetch(:user, {}).permit(:name, :email, :password, :password_confirmation)
     end
